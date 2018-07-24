73236a712dbd85b557c0ea1f40d5323e068561ab
Julian Miller <julian.miller@orainteractive.com>
2018-07-23 20:56:13 -0500

    Update

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ora-data-loading',
     environment,
     rootURL: '/ora-data-loading',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
