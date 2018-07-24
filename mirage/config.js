ce0c09c373ac981b6f216e81c7c5d5cdb64b0bb3
Julian Miller <julian.miller@orainteractive.com>
2018-07-23 20:38:55 -0500

    Update

--- mirage/config.js
+++ mirage/config.js
@@ -8,7 +8,7 @@ export default function() {
     Note: these only affect routes defined *after* them!
   */
 
-  this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = 'https://jmilleroi.github.io/ora-data-loading';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   this.timing = 3000;      // delay for each request, automatically set to 0 during testing
 
